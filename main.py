5cc0290315d1c438162046df5f03a2c06aba7664
MonkeyMax123 <98365388+MonkeyMax123@users.noreply.github.com>
2022-02-16 21:14:05 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,75 @@
-def foo(x):
-    if x <= 1:
-        return x
-    else:
-        n = foo(x-1) + foo(x-2)
-        return n
+import tabulate
+import time
 
 
-def longest_run(mylist, key):
-    ### TODO
-    pass
+def linear_search(mylist, key):
+    for i, v in enumerate(mylist):
+        if v == key:
+            return i
+    return -1
 
 
-class Result:
-    """ done """
-
-    def __init__(self, left_size, right_size, longest_size, is_entire_range):
-        self.left_size = left_size  # run on left side of input
-        self.right_size = right_size  # run on right side of input
-        self.longest_size = longest_size  # longest run in input
-        self.is_entire_range = is_entire_range  # True if the entire input matches the key
-
-    def __repr__(self):
-        return ('longest_size=%d left_size=%d right_size=%d is_entire_range=%s' %
-                (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
+def test_linear_search():
+    assert linear_search([1, 2, 3, 4, 5], 5) == 4
+    assert linear_search([1, 2, 3, 4, 5], 1) == 0
+    assert linear_search([1, 2, 3, 4, 5], 6) == -1
 
 
-def longest_run_recursive(mylist, key):
-    ### TODO
-    pass
+def binary_search(mylist, key):
+    return _binary_search(mylist, key, 0, len(mylist) - 1)
 
 
-## Feel free to add your own tests here.
-def test_longest_run():
-    assert longest_run([2, 12, 12, 8, 12, 12, 12, 0, 12, 1], 12) == 3
+def _binary_search(mylist, key, left, right):
+    mid = int(((left + right) / 2))
+    if key == mylist[mid]:
+        return mid
+    elif left >= right:
+        return -1
+    elif key > mylist[mid]:
+        return _binary_search(mylist, key, mid + 1, right)
+    elif key < mylist[mid]:
+        return _binary_search(mylist, key, left, mid - 1)
 
 
-result = foo(10)
-print(result)
+def test_binary_search():
+    assert binary_search([1, 2, 3, 4, 5], 5) == 4
+    assert binary_search([1, 2, 3, 4, 5], 1) == 0
+    assert binary_search([1, 2, 3, 4, 5], 6) == -1
+    assert binary_search([1, 2, 3, 4, 5], 3) == 2
+    assert binary_search([1, 2, 3, 4, 5], -1) == -1
+
+
+def time_search(search_fn, mylist, key):
+    time_start = time.time()
+    search_fn(mylist, key)
+    time.sleep(.001)
+    time_end = time.time()
+    time_total = (time_end - time_start - .001) * 1000
+    return time_total
+
+
+def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
+    search_times = []
+
+    for i in range(len(sizes)):
+        sample = range(1, int(sizes[i]))
+        search_times.append(
+            (int(sizes[i]), time_search(linear_search, sample, -1), time_search(binary_search, sample, -1)))
+
+    return search_times
+
+
+def print_results(results):
+    print(tabulate.tabulate(results,
+                            headers=['n', 'linear', 'binary'],
+                            floatfmt=".3f",
+                            tablefmt="github"))
+
+
+def test_compare_search():
+    res = compare_search(sizes=[10, 100])
+    print(res)
+    assert res[0][0] == 10
+    assert res[1][0] == 100
+    assert res[0][1] < 1
+    assert res[1][1] < 1
